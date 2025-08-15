36c5a08db95e871784127317abd3fb94668f5946
NotAlexNoyle <alexnoyle@icloud.com>
2025-08-15 03:02:39 -0400

    Update gradle to 8.14.3, add formatting

--- src/main/java/net/trueog/horsetpog/HorseTpOG.java
+++ src/main/java/net/trueog/horsetpog/HorseTpOG.java
@@ -20,44 +20,63 @@ public final class HorseTpOG extends JavaPlugin {
 
     @Override
     public void onLoad() {
+
         // add the WorldGuard flag for an area where HorseTp will not work
         FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
         try {
+
             // create a flag with the name "my-custom-flag", defaulting to true
             StateFlag flag = new StateFlag("allow-boat-tp", true);
             registry.register(flag);
             HorseTpFlag = flag; // only set our field if there was no error
+
         } catch (FlagConflictException e) {
+
             // some other plugin registered a flag by the same name already.
-            // you can use the existing flag, but this may cause conflicts - be sure to check type
+            // you can use the existing flag, but this may cause conflicts - be sure to
+            // check type
             Flag<?> existing = registry.get("allow-boat-tp");
             if (existing instanceof StateFlag) {
+
                 HorseTpFlag = (StateFlag) existing;
+
             } else {
+
                 // types don't match - this is bad news! some other plugin conflicts with you
                 // hopefully this never actually happens
             }
+
         }
+
     }
 
     @Override
     public void onEnable() {
+
         plugin = this;
         // Plugin startup logic
 
         getServer().getPluginManager().registerEvents(new OnEntityDismount(), this);
         getServer().getPluginManager().registerEvents(new OnPlayerTeleport(), this);
+
     }
 
     public static HorseTpOG getPlugin() {
+
         return plugin;
+
     }
 
     public static StateFlag getHorseTpFlag() {
+
         return HorseTpFlag;
+
     }
 
     public static HashMap<Player, Entity> getVehicleCache() {
+
         return vehicleCache;
+
     }
+
 }
--- src/main/java/net/trueog/horsetpog/Listeners/OnEntityDismount.java
+++ src/main/java/net/trueog/horsetpog/Listeners/OnEntityDismount.java
@@ -15,6 +15,7 @@ public class OnEntityDismount implements Listener {
     // when a player teleports, they are dismounted from their vehicle
     // so when they get dismounted, teleport the vehicle to them
     public void onEntityDismount(EntityDismountEvent event) {
+
         if (event.getEntity() instanceof Player) {
 
             // When a player dismounts, add them and the vehicle
@@ -25,13 +26,14 @@ public class OnEntityDismount implements Listener {
             HorseTpOG.getVehicleCache().put(player, vehicle);
 
             // 5 ticks later, remove them from the cache
-            Bukkit.getScheduler()
-                    .runTaskLater(
-                            HorseTpOG.getPlugin(),
-                            () -> {
-                                HorseTpOG.getVehicleCache().remove(player);
-                            },
-                            5);
+            Bukkit.getScheduler().runTaskLater(HorseTpOG.getPlugin(), () -> {
+
+                HorseTpOG.getVehicleCache().remove(player);
+
+            }, 5);
+
         }
+
     }
+
 }
